9d8864d95dae5540b22a1ced09a38fea37e04c6e
chaudharygarvit9 <chaudharygarvit9@gmail.com>
2021-04-14 07:50:10 +0530

    Initial Commit

--- StringMatchingAlgorithms/main.cpp
+++ StringMatchingAlgorithms/main.cpp
@@ -6,9 +6,21 @@
 //
 
 #include <iostream>
-
+#include<string>
+#include<fstream>
+using namespace std;
+void printline(){
+    cout<<"\n=============================================================================\n";
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    printline();
+    cout<<"\nWe will be implementing following algorithms:\n1.KMP\n2.Rabin Karp\n3.Automata\n4.Boyer Moore\n5.Naive\n";
+    printline();
     return 0;
 }
+
+// Program to generate text string
+// Store output in a file to run for different algorithms
+
